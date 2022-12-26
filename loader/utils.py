31563efd1150396afe7f336f54261caccfa88207
Petr Matis <dragon031088@gmail.com>
2022-12-26 13:45:07 +0300

    lesson12

---
+++ loader/utils.py
@@ -0,0 +1,8 @@
+def save_picture(picture):
+    filename = picture.filename
+    file_type = filename.split('.')[-1]
+    if file_type not in ('jpeg', 'svg', 'bmp', 'png'):
+        return
+    picture.save(f'./uploads/{filename}')
+    return f'uploads/{filename}'
+
